d86d84189cd706e2678525758caa1330d4aca523
Victor Ivanov <viktorivanofs@gmail.com>
2026-04-02 19:09:28 +0400

    Selenium WebDriver Exercise
    Selenium WebDriver Exercise

---
+++ More Exercises/02. First Test/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿global using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _02._First_Test
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // take out url separately in a variable
+            driver.Url = "https://nakov.com.";
+            var windowTitle = driver.Title;
+            Assert.That(windowTitle.Contains("Svetlin Nakov – Official Web Site"));
+            Console.WriteLine(windowTitle);
+
+            var searchLink = driver.FindElement(By.ClassName("smoothScroll"));
+            //Find the search link by its class name and verify its text contains "SEARCH".
+            Assert.That(searchLink.Text.Contains("SEARCH"));
+            Console.WriteLine(searchLink.Text);
+
+            searchLink.Click();
+
+            var message = driver.FindElement(By.Id("s"));
+            //Retrieve and verify the placeholder attribute of the search input is "Search this site".
+            var placeholderText = message.GetAttribute("placeholder");
+            Assert.That(placeholderText, Is.EqualTo("Search this site"));
+            Console.WriteLine(placeholderText);
+        }
+
+            [TearDown]
+            public void TearDown()
+            {
+                driver.Dispose();
+
+            }
+    }
+}
---
+++ More Exercises/03. Practice Locators/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿global using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V143.Network;
+
+
+namespace _03._Practice_Locators
+{
+    public class Tests
+    {
+
+        private IWebDriver driver;
+        private string baseUrl = "C:\\Users\\solev\\OneDrive\\Desktop\\Front-End Test Automation - October 2025\\03. Selenium WebDriver\\My Solutions\\03. Practice Locators\\SimpleForm\\Locators.html";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            driver.FindElement(By.Id("lname"));
+            driver.FindElement(By.Name("newsletter"));
+            driver.FindElement(By.TagName("a"));
+            driver.FindElement(By.ClassName("information"));
+        }
+
+        [Test]
+        public void Test2()
+        { 
+            driver.FindElement(By.LinkText("SoftUni Official Page"));
+            driver.FindElement(By.PartialLinkText("Official Page"));
+
+        }
+
+        public void Test3()
+        {
+            driver.FindElement(By.CssSelector("#fname"));
+            driver.FindElement(By.CssSelector("input[name='fname']"));
+            driver.FindElement(By.CssSelector("input[class*='button']"));
+            driver.FindElement(By.CssSelector("div.additional-info > p > input[type='text']"));
+            driver.FindElement(By.CssSelector("form div.additional-info input[type='text']"));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            driver.FindElement(By.XPath("/html/body/form/input[1]"));
+            driver.FindElement(By.XPath("//input[@value='m']"));
+            driver.FindElement(By.XPath("//input[@name='lname']"));
+            driver.FindElement(By.XPath("//input[@type='checkbox']"));
+            driver.FindElement(By.XPath("//input[@class='button']"));
+            driver.FindElement(By.XPath("//div[@class='additional-info']//input[@type='text']"));
+        }
+
+        [OneTimeTearDown]
+
+        public void TearDown()
+        {
+            driver.Dispose();
+        }
+
+    }
+}
